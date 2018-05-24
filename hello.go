05731ecc711c67860b95b88576fe9cb65d496087
freewind <nowindlee@gmail.com>
2018-05-24 23:34:08 +0800

    panic demo

--- hello.go
+++ hello.go
@@ -2,6 +2,14 @@ package main
 
 import "fmt"
 
+type X interface{}
+type A struct{}
+type B struct{}
+
 func main() {
-	fmt.Println("Hello go!")
+	fmt.Println("Hello")
+	var x X
+	x = A{}
+	b := x.(B) // will panic, good
+	fmt.Println(b)
 }
